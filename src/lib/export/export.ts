2ef3eb8f31b92f1688dd9c57d868cdcc48d3ae1f
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-03-12 16:39:46 -0500

    refactor(export): switch axios with fetch
    include logs

--- src/lib/export/export.ts
+++ src/lib/export/export.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
-import axios from '@/lib/axios';
+import { env } from '@/lib/env';
 import { ExportType } from '@/types/export';
 
 export const fetchExport = async (
@@ -8,8 +8,27 @@ export const fetchExport = async (
   sent: boolean = false
 ): Promise<ExportType> => {
   const session = await getServerSession(authOptions);
-  const headers = session ? { Authorization: `Bearer ${session.refreshToken}` } : {};
+  if (!session || !session.refreshToken) {
+    throw new Error('No se pudo obtener una sesión válida o el token no está disponible.');
+  }
+
   const endpoint = sent ? `/export/export-sent/${exportId}` : `/export/${exportId}`;
-  const response = await axios.get<ExportType>(endpoint, { headers });
-  return response.data;
+  const url = env.NEXT_PUBLIC_API_URL + endpoint;
+  
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session.refreshToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    console.error('Error al obtener export:', errorData);
+    throw new Error(`Error al obtener export: ${response.statusText}`);
+  }
+
+  const data = (await response.json()) as ExportType;
+  return data;
 };
