e91c3acfeae559d1e3563b3444bb2b13829d3cce
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-03-28 18:16:44 -0500

    feat(shipping-company): implementation of component flow

---
+++ src/components/shipping-company/ShippingCompanySelectBase.tsx
@@ -0,0 +1,90 @@
+import { Icon } from '@chakra-ui/react';
+import {
+    ChakraStylesConfig,
+    DropdownIndicatorProps,
+    Select as ChakraSelect,
+    GroupBase,
+    SingleValue,
+    chakraComponents,
+    CSSObjectWithLabel,
+} from 'chakra-react-select';
+import React, { useEffect } from 'react';
+import { MdOutlineArrowDropDownCircle } from 'react-icons/md';
+import { ShippingCompanyType } from '../../types/shippingCompany';
+import { useShippingCompanies } from '../../hooks/export/shippingCompany/getShippingCompanies';
+import { usePagination } from '../../hooks/usePagination';
+import { useRouter } from 'next/navigation';
+
+interface ShippingCompanySelectBaseProps {
+    name: string;
+    placeholder: string;
+    onChange: (newValue: Partial<ShippingCompanyType> | null) => void;
+}
+
+const chakraStyles: ChakraStylesConfig<Partial<ShippingCompanyType>, false, GroupBase<Partial<ShippingCompanyType>>> = {
+    container: (provided) => ({ ...provided, w: 'full' }),
+    placeholder: (provided) => ({ ...provided, color: 'gray.600' }),
+    input: (provided) => ({ ...provided, color: 'gray.900' }),
+    option: (provided) => ({
+        ...provided,
+        borderBottom: '1px solid',
+        borderColor: 'gray.200',
+    }),
+};
+
+const shippingCompanyComponents = {
+    DropdownIndicator: (
+        props: DropdownIndicatorProps<Partial<ShippingCompanyType>, false, GroupBase<Partial<ShippingCompanyType>>>
+    ): JSX.Element => (
+        <chakraComponents.DropdownIndicator {...props}>
+            <Icon as={MdOutlineArrowDropDownCircle} size="13px" />
+        </chakraComponents.DropdownIndicator>
+    ),
+};
+
+const ShippingCompanySelectBase: React.FC<ShippingCompanySelectBaseProps> = ({ name, placeholder, onChange }) => {
+    const { paginationParams } = usePagination();
+    const { data = [], isLoading, error } = useShippingCompanies({
+        ...paginationParams,
+    });
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!!error) {
+            const { response } = error as any;
+            const { data } = response;
+            const { statusCode } = data;
+
+            if (statusCode === 401) {
+                router.push('/api/auth/signout');
+            }
+        }
+    }, [error]);
+
+
+    const handleChange = (newValue: SingleValue<Partial<ShippingCompanyType>>): void => {
+        onChange(newValue);
+    };
+
+    return (
+        <ChakraSelect
+            name={name}
+            menuPortalTarget={document.body}
+            styles={{
+                menuPortal: (provided) => ({ ...provided, zIndex: 100 }) as CSSObjectWithLabel,
+            }}
+            useBasicStyles
+            chakraStyles={chakraStyles}
+            noOptionsMessage={() => error ? 'Error al cargar navieras' : 'No hay navieras disponibles'}
+            isLoading={isLoading}
+            options={data}
+            getOptionLabel={(opt: Partial<ShippingCompanyType>) => opt.name || ''}
+            getOptionValue={(opt: Partial<ShippingCompanyType>) => opt.id ? opt.id.toString() : ''}
+            onChange={(newValue) => handleChange(newValue)}
+            placeholder={placeholder}
+            components={shippingCompanyComponents}
+        />
+    );
+};
+
+export default ShippingCompanySelectBase;
