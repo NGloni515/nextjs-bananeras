052665040c21c207b20d47a4dd6e5204cecdbd40
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-04-08 21:27:39 -0500

    feat(dashboard): setup initial dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,6 @@
 'use client';
-import { Box, Center, Flex } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import TableExport from '../../components/export/table-export/TableExport';
-import TableProducers from '../../components/producer/table-producer/TableProducers';
+import Dashboard from '../../components/dashboard/Dashboard';
 import IsOnboarding from '../../components/ui/IsOnboarding';
 
 interface WindowSizeProps {
@@ -28,35 +26,7 @@ const DashboardPage = (): JSX.Element => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return (
-    <Box
-      my={'33px'}
-      mx={'auto'}
-      width={{
-        sm: `${Number(windowSize.width) - 20}px`,
-        md: `${Number(windowSize.width) - 300}px`,
-      }}
-    >
-      <Center>
-        <Flex flexDirection={'column'} width='100%' py={'4px'} gap={'20px'}>
-          <TableProducers
-            windowSize={windowSize}
-            width={{
-              sm: Number(windowSize.width) - 20,
-              md: Number(windowSize.width) - 300,
-            }}
-          />
-          <TableExport
-            windowSize={windowSize}
-            width={{
-              sm: Number(windowSize.width) - 20,
-              md: Number(windowSize.width) - 300,
-            }}
-          />
-        </Flex>
-      </Center>
-    </Box>
-  );
+  return <Dashboard windowSize={windowSize} />;
 };
 
 export default IsOnboarding(DashboardPage);
