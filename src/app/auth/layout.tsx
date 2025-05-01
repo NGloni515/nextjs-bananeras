9699e77fbd4bd1ec7e85b8ab59bda1e7a5eb14df
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-05-01 16:51:09 -0500

    fix(layout): updated main view

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -21,33 +21,45 @@ export default function AuthLayout({
           '2xl': 'block',
         }}
       >
-        <Image src='/banano-pic.jpg' alt='Imagen' fill />
+        <Image src='/banano-pic.png' alt='Imagen' fill />
         <Box
           position='absolute'
-          top='50%'
-          left='50%'
-          transform='translate(-50%, -50%)'
-          bg={'rgba(0, 128, 0, 0.92)'}
-          opacity='1'
-          w={{ '2xl': '560px', xl: '490px' }}
-          h={{ '2xl': '570px', xl: '440px' }}
-          py={{ xl: '56px', '2xl': '84px' }}
-          pl={{ xl: '64px', '2xl': '50px' }}
-          pr={{ xl: '64px', '2xl': '136px' }}
+          top='0'
+          left='0'
+          width='100%'
+          py={{ xl: '24px', '2xl': '24px' }}
+          px={'32px'}
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          bgGradient='linear(to-b, rgba(40, 130, 93, 0.92), rgba(40, 130, 93, 0))'
         >
           <Text
             fontSize={{
-              xl: theme.fontSizes['4xl'],
-              '2xl': theme.fontSizes['5xl'],
+              xl: theme.fontSizes['3xl'],
+              '2xl': theme.fontSizes['4xl'],
             }}
             fontWeight={{
               xl: theme.fontWeights.extrabold,
               '2xl': theme.fontWeights.extrabold,
             }}
+            textAlign='center'
+            color='white'
           >
-            Mejora continuamente tu producción de banano
+            Gestiona tus exportaciones con control total y trazabilidad en
+            tiempo real
           </Text>
         </Box>
+        <Box
+          position='absolute'
+          bottom='0'
+          left='0'
+          width='100%'
+          py={'24px'}
+          px={'24px'}
+          display='flex'
+          bgGradient='linear(to-b, rgba(40, 130, 93, 0), rgba(40, 130, 93, 0.92))'
+        ></Box>
       </Box>
 
       {/* Parte derecha */}
