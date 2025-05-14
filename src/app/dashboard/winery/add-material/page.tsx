b98ffe84f1e8c8a21b71d4f78316930a71cbf783
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-05-13 23:29:55 -0500

    fix(dashboard): update add material page

--- src/app/dashboard/winery/add-material/page.tsx
+++ src/app/dashboard/winery/add-material/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   Box,
   Card,
@@ -7,35 +8,50 @@ import {
   Center,
   Heading,
 } from '@chakra-ui/react';
+import { Formik } from 'formik';
 import React from 'react';
 import IsOnboarding from '../../../../components/ui/IsOnboarding';
+import ImportExporterStockDrawer from '../../../../components/winery/ImportExporterStockDrawer';
 import { MaterialList } from '../../../../components/winery/MaterialList';
 
 function AddMaterialPage(): React.JSX.Element {
   return (
-    <>
-      <Box my={'20px'} mx={'auto'} w={'95%'}>
-        <Center>
-          <Card
-            w={{
-              base: '95%',
-              sm: '95%',
-              md: '90%',
-              lg: '100%',
-              xl: '100%',
-            }}
-            mb={'20px'}
-          >
-            <CardHeader w={'100%'}>
-              <Heading>Agregar Stock de Materiales</Heading>
-            </CardHeader>
-            <CardBody w='100%'>
-              <MaterialList />
-            </CardBody>
-          </Card>
-        </Center>
-      </Box>
-    </>
+    <Formik
+      initialValues={{ 'import-exporter-stock': null }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {() => (
+        <Box my={'20px'} mx={'auto'} w={'95%'}>
+          <Center>
+            <Card
+              w={{
+                base: '95%',
+                sm: '95%',
+                md: '90%',
+                lg: '100%',
+                xl: '100%',
+              }}
+              mb={'20px'}
+            >
+              <CardHeader
+                w={'100%'}
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'
+              >
+                <Heading>Agregar Stock de Materiales</Heading>
+                <ImportExporterStockDrawer />
+              </CardHeader>
+              <CardBody w='100%'>
+                <MaterialList />
+              </CardBody>
+            </Card>
+          </Center>
+        </Box>
+      )}
+    </Formik>
   );
 }
 
