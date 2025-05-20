63487ed599b77af4cbf130ceee4e03a091fb3a55
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-05-20 14:26:58 -0500

    fix(middleware): middleware testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,10 +9,13 @@ const roleBasedRoutes = [
   { path: '/dashboard/box-brands/cutting-types', roles: ['QUALITY'] },
   { path: '/dashboard/export/cutting-sheets', roles: ['QUALITY'] },
   { path: '/dashboard/box-brands/verifiers', roles: ['QUALITY'] },
+
   { path: '/dashboard/liquidation', roles: ['ADMINISTRATIVE'] },
+
   { path: '/dashboard/user/update-user', roles: ['ADMINISTRATIVE'] },
   { path: '/dashboard/settings', roles: ['ADMINISTRATIVE'] },
   { path: '/dashboard/producer/upload-logo', roles: ['ADMINISTRATIVE'] },
+
   { path: '/dashboard/producer', roles: ['LOGISTICS', 'EXPORT'] },
   { path: '/dashboard/client', roles: ['LOGISTICS', 'EXPORT'] },
   { path: '/dashboard/box-brands', roles: ['LOGISTICS', 'EXPORT'] },
@@ -22,39 +25,23 @@ const roleBasedRoutes = [
 export async function middleware(
   req: NextRequest
 ): Promise<NextResponse<unknown>> {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
-  console.log('[Middleware] Requested path:', pathname);
-
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-    secureCookie: process.env.NODE_ENV === 'production',
-  });
-
-  console.log('[Middleware] Token:', token);
-
   if (!token) {
-    console.log('[Middleware] No token, redirecting to signin');
     const signInUrl = new URL('/auth/signin', req.url);
     signInUrl.searchParams.set('callbackUrl', req.url);
     return NextResponse.redirect(signInUrl);
   }
 
-  const userRole = token?.user?.role;
-  console.log('[Middleware] User role:', userRole);
-
+  const userRole = token.user?.role;
   if (!userRole) {
-    console.log(
-      '[Middleware] No role found in token, redirecting to dashboard with error'
-    );
     const unauthorizedUrl = new URL('/dashboard', req.url);
     unauthorizedUrl.searchParams.set('error', 'unauthorized');
     return NextResponse.redirect(unauthorizedUrl);
   }
 
   if (userRole === 'MASTER') {
-    console.log('[Middleware] Role is MASTER, granting access');
     return NextResponse.next();
   }
 
@@ -62,18 +49,12 @@ export async function middleware(
     pathname.startsWith(route.path)
   );
 
-  console.log('[Middleware] Route config:', routeConfig);
-
   if (routeConfig && !routeConfig.roles.includes(userRole)) {
-    console.log(
-      '[Middleware] Role not allowed, redirecting to dashboard with error'
-    );
     const unauthorizedUrl = new URL('/dashboard', req.url);
     unauthorizedUrl.searchParams.set('error', 'unauthorized');
     return NextResponse.redirect(unauthorizedUrl);
   }
 
-  console.log('[Middleware] Access granted');
   return NextResponse.next();
 }
 
