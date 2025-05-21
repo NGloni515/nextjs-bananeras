3f31e3710182d4444db843c8b94f607222e99c8a
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-05-21 18:06:56 -0500

    fix(middleware): middleware testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,8 @@
 import { NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(
-  req: NextRequest
-): Promise<NextResponse<unknown>> {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-    secureCookie: true,
-  });
-
-  const pathname = req.nextUrl.pathname;
-
-  console.log('[Middleware] Requested path:', pathname);
-  console.log('[Middleware] Token:', token);
-  console.log('[Middleware] User role:', token?.user?.role);
-  console.log('[Middleware] Onboarding status:', token?.user?.onboardingStatus);
-
+export function middleware(req: NextRequest): NextResponse {
+  console.log('[Middleware] ejecutado:', req.nextUrl.pathname);
   return NextResponse.next();
 }
 
