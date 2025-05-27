a809eea236b568d15c081c165908d9e1c95b03d2
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-05-27 14:06:22 -0500

    feat(location): add components for City, Country, and Province select inputs

--- src/components/location/InputFieldCitySelect.tsx
+++ src/components/location/InputFieldCitySelect.tsx
@@ -4,41 +4,44 @@ import React from 'react';
 import CitySelectBase from './CitySelectBase';
 
 interface InputFieldCitySelectProps {
-    name: string;
-    label: string;
-    placeholder: string;
-    provinceId?: number;
+  name: string;
+  label: string;
+  placeholder: string;
+  provinceId?: number;
+  resetOnParentChange?: boolean;
 }
 
 const InputFieldCitySelect: React.FC<InputFieldCitySelectProps> = ({
-    name,
-    label,
-    placeholder,
-    provinceId,
+  name,
+  label,
+  placeholder,
+  provinceId,
+  resetOnParentChange = true,
 }) => {
-    const [field, meta, helpers] = useField(name);
+  const [field, meta, helpers] = useField(name);
 
-    return (
-        <FormControl id={name} isInvalid={!!meta.error && meta.touched}>
-            <FormLabel fontSize="sm" mb="8px" h={'21px'}>
-                {label}
-            </FormLabel>
+  return (
+    <FormControl id={name} isInvalid={!!meta.error && meta.touched}>
+      <FormLabel fontSize='sm' mb='8px' h={'21px'}>
+        {label}
+      </FormLabel>
 
-            <CitySelectBase
-                provinceId={provinceId}
-                name={name}
-                placeholder={placeholder}
-                field={field}
-                onChange={(newValue) => helpers.setValue(newValue?.id || '')}
-            />
+      <CitySelectBase
+        provinceId={provinceId}
+        name={name}
+        placeholder={placeholder}
+        field={field}
+        resetOnParentChange={resetOnParentChange}
+        onChange={(newValue) => helpers.setValue(newValue?.id || '')}
+      />
 
-            {meta.error && meta.touched && (
-                <FormErrorMessage mt="8px" mb="16px">
-                    {meta.error}
-                </FormErrorMessage>
-            )}
-        </FormControl>
-    );
+      {meta.error && meta.touched && (
+        <FormErrorMessage mt='8px' mb='16px'>
+          {meta.error}
+        </FormErrorMessage>
+      )}
+    </FormControl>
+  );
 };
 
 export default InputFieldCitySelect;
--- src/components/location/InputFieldCountrySelect.tsx
+++ src/components/location/InputFieldCountrySelect.tsx
@@ -2,31 +2,37 @@ import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/react';
 import { useField } from 'formik';
 import React from 'react';
 import CountrySelectBase from './CountrySelectBase';
+import { Country } from '../../types/location/country';
 
 interface InputFieldCountrySelectProps {
   name: string;
   label: string;
   placeholder: string;
+  onChange?: (newValue: Country | null) => void;
 }
 
 const InputFieldCountrySelect: React.FC<InputFieldCountrySelectProps> = ({
   name,
   label,
   placeholder,
+  onChange,
 }) => {
   const [field, meta, helpers] = useField(name);
 
   return (
     <FormControl id={name} isInvalid={!!meta.error && meta.touched}>
       <FormLabel fontSize='sm' mb='8px' h={'21px'}>
-        {label} 
+        {label}
       </FormLabel>
 
       <CountrySelectBase
         name={name}
         placeholder={placeholder}
-        onChange={(newValue) => helpers.setValue(newValue?.id)}
         field={field}
+        onChange={(newValue) => {
+          helpers.setValue(newValue?.id || '');
+          onChange?.(newValue || null);
+        }}
       />
 
       {meta.error && meta.touched && (
