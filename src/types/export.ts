4a93e2b256a47bfd6120f2789aef314b1e78f145
madlover2001 <63275589+madlover2001@users.noreply.github.com>
2025-06-06 10:29:39 -0500

    fix(export): update export response and types definitions

--- src/types/export.ts
+++ src/types/export.ts
@@ -29,7 +29,7 @@ export type ExportType = {
   shipName: string;
   estimatedTravelTime: string;
   bookingNumber: string;
-  cutOffTime: string;
+  shippingDateTime: Date | '';
   shippingCompanyId: number;
   departureHarborId: number;
   destinationHarborId: number;
